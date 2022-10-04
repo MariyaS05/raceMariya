0bfd617c5c66f4e630f82376c5467aedbb6ba64f
MariyaS05 <mariya_govorushko@mail.ru>
2022-10-04 22:42:30 +0300

    на этом мои полномочия все..

---
+++ raceMariya/extension+Labels.swift
@@ -0,0 +1,15 @@
+//
+//  extension+Labels.swift
+//  raceMariya
+//
+//  Created by Мария  on 4.10.22.
+//
+
+import Foundation
+import UIKit
+
+extension UILabel {
+    func makeLabel (){
+        textColor = UIColor(red: 254/255, green: 195/255, blue: 69/255, alpha: 1)
+    }
+}
